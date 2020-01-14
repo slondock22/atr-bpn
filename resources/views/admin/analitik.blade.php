957a42b64e485067e724f4b9ff1e2b1019a670cf
Airlangga Pasmika <airlangga.pasmika@edi-indonesia.co.id>
2020-01-14 22:29:05 +0700

    update dns sisense in analitik

--- resources/views/admin/analitik.blade.php
+++ resources/views/admin/analitik.blade.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
     <div class="tb-height-b30 tb-height-lg-b30"></div>
       <div class="row">
-        <iframe id="ifm" name="ifm" width="100%" height="1120px" frameborder="0" src="http://103.234.195.130:8081/app/main#/dashboards/5df0f23e100c8c1110b2d08d?embed=true" scrolling="auto"></iframe>
+        <iframe id="ifm" name="ifm" width="100%" height="1120px" frameborder="0" src="http://sisense.edi-indonesia.co.id:8081/app/main#/dashboards/5df0f23e100c8c1110b2d08d?embed=true" scrolling="auto"></iframe>
       </div><!-- .row -->
     </div>
     <!-- .container-fluid -->
