047f60d3dce827717045ab9973f13b7bb2c154ed
Airlangga Pasmika <airlangga.pasmika@edi-indonesia.co.id>
2020-01-15 13:29:09 +0700

    update dokumentasi link

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -26,7 +26,7 @@
             <div class="tb-cta-text">
               <h2 class="tb-cta-title">Selamat Datang di Portal Admin #TanyaATRBPN</h2>
               <div class="tb-cta-subtitle">Portal ini bertujuan untuk mengelola segala pertanyaan dan aduan dari masyarakat, untuk mengatur disposisi aduan hingga mengunggah berita terbaru dari Kementerian Agraria dan Tata Ruang/Badan Pertanahan Nasional. <br> Klik link dibawah untuk cara penggunaan.</div>
-              <div class="tb-cta-btn"><a href="#" class="tb-btn tb-style3 tb-color3">Dokumentasi</a></div>
+              <div class="tb-cta-btn"><a href="{{route('dokumentasi')}}" class="tb-btn tb-style3 tb-color3">Dokumentasi</a></div>
             </div>
           </div><!-- .tb-cta -->
         </div>
