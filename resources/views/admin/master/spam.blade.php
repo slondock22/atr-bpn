eaa474107e4373cbf12436751a234926bf3cc21a
Syarief Hidayatullah <sharifadamz@gmail.com>
2020-02-20 15:27:05 +0700

    Update Jenis Aduan Ajax

--- resources/views/admin/master/spam.blade.php
+++ resources/views/admin/master/spam.blade.php
@@ -3,105 +3,30 @@
 @section('content')
 <div class="tb-content tb-style1">
   <div class="tb-padd-lr-30 tb-uikits-heading">
-    <h2 class="tb-uikits-title">Folder Spam</h2>
+    <h2 class="tb-uikits-title">Spam</h2>
     </ul>
   </div>
   <div class="tb-height-b30 tb-height-lg-b30"></div>
   <div class="container-fluid">
-    <div class="row">
+    <div class="row"> 
       <div class="col-lg-12">
         <div class="tb-card tb-style1">
           <div class="tb-card-heading">
-            <div class="tb-card-heading-right">
-              <form action="#" class="tb-search tb-style2">
-                <input type="text" placeholder="Search..." class="tb-search-input">
-                <button type="submit"><i class="material-icons-outlined">search</i></button>
-              </form>
-              <a href="#" class="tb-btn tb-style1 tb-small">View All</a>
-            </div>
           </div>
+
           <div class="tb-card-body">
-            <div class="tb-table tb-style1 tb-type1 table-responsive">
-              <table class="table">
+             <table class="display" id="datatable" width="100%">
                 <thead>
                   <tr>
-                    <th>#</th>
-                    <th>Username</th>
-                    <th>Tgl Post</th>
-                    <th>Konten</th>
-                    <th>Sumber</th>
-                    <th>Aksi</th>{{-- 
-                    <th></th> --}}
+                    <th>ID</th>
+                    <th width="300">Ministry Name</th>
+                    <th width="200">City</th>
+                    <th width="200">Address</th>
+                    <th width="200">Phone</th>
+                    <th width="100">Aksi</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>juragankodok</td>
-                    <td>27 Nov 2019</td>
-                    <td>ichi shuan meh lon geh kam bun yah maka nsai sua naplaio</td>
-                    <td>Twitter</td>
-                     <td>
-                        <a class="tb-solial-btn social-derault-color tb-radious50">
-                            <i class="lni lni-trash"></i>
-                        </a>
-                        <a class="tb-solial-btn social-derault-color tb-radious50">
-                            <i class="lni lni-reload"></i>
-                        </a>
-                    </td>
-                    {{-- <td>
-                      <div class="tb-table-more-option text-center"><i class="material-icons-outlined">more_horiz</i></div>
-                    </td> --}}
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td>tukangbubur</td>
-                    <td>28 Nov 2019</td>
-                    <td>2219092092920190 09290219029021 09219012902190</td>
-                    <td>Twitter</td>
-                     <td>
-                        <a class="tb-solial-btn social-derault-color tb-radious50">
-                            <i class="lni lni-trash"></i>
-                        </a>
-                        <a class="tb-solial-btn social-derault-color tb-radious50">
-                            <i class="lni lni-reload"></i>
-                        </a>
-                    </td>
-                    {{-- <td>
-                      <div class="tb-table-more-option text-center"><i class="material-icons-outlined">more_horiz</i></div>
-                    </td> --}}
-                  </tr>
-             
-                </tbody>
               </table>
-            </div><!-- .tb-table -->
-            <div class="tb-table-footer">
-              <div class="tb-table-footer-left">
-                <div class="tb-table-footer-left-text">This report was generated on 29 Des,2019 at 9:34:19 PM - <a href="#">Refresh Report</a></div>
-              </div>
-              <div class="tb-table-footer-right">
-                <ul class="tb-mp0 tb-table-footer-list">
-                  <li>
-                    <span class="tb-efl-title">Show Rows:</span>
-                    <div class="tb-custom-select-wrap tb-style1">
-                      <select name="#" class="tb-custom-select">
-                        <option value="classic-fit1">10</option>
-                        <option value="classic-fit2">15</option>
-                        <option value="classic-fit3">20</option>
-                      </select>
-                    </div>
-                  </li>
-                  <li><span class="tb-efl-title">Go to</span><input type="text" value="1"></li>
-                  <li>1 - 10 of 10</li>
-                  <li>
-                    <div class="tb-navigation tb-style1">
-                      <a href="#" class="tb-prev"><i class="material-icons-outlined">keyboard_arrow_left</i></a>
-                      <a href="#" class="tb-next"><i class="material-icons-outlined">keyboard_arrow_right</i></a>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </div>
           </div>
         </div>
       </div><!-- .col -->
@@ -109,4 +34,46 @@
   </div>
   <div class="tb-height-b60 tb-height-lg-b60"></div>
 </div>
+
+
 @endsection
+<script src="{{asset('/assets-back')}}/js/vendor/jquery-1.12.4.min.js"></script>
+<script>
+
+  $(document).ready(function(){
+    getDataSpam();
+  });
+
+    function getDataSpam(){
+          var table = $('#datatable').DataTable({
+                 dom: "rtiplf",
+                 language: {
+                    searchPlaceholder: "Search..."
+                },
+                 processing: true,
+                 serverSide: true,
+                 destroy:true,
+                 ajax: 'master/ministry',
+                 columns: [
+                      { data: 'id', name: 'id' },
+                      { data: 'name', name: 'name' },
+                      { data: 'city', name: 'city' },
+                      { data: 'address', name: 'address' },
+                      { data: 'phone_1', name: 'phone_1' },
+                      {
+                        data: 'id',
+                        render: function(data){
+                          
+                          return '<a onclick="delData('+data+')" class="tb-solial-btn social-derault-color tb-radious50">\
+                                                <i class="lni lni-trash"></i>\
+                                            </a>\
+                                            <a onclick="edit('+data+')" class="tb-solial-btn social-derault-color tb-radious50">\
+                                                <i class="lni lni-pencil"></i>\
+                                            </a>';
+                        }
+                      }
+                  ],
+          });
+    }
+
+</script>
