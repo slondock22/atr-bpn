221a09f9212f3f4eb69db8b23da925219a998f64
User Aplikasi <webapp@atrbpn.go.id>
2020-07-20 21:47:43 +0700

    analitik update

--- resources/views/admin/analitik.blade.php
+++ resources/views/admin/analitik.blade.php
@@ -13,7 +13,7 @@ setcookie($cookie_name, $cookie_value, time()+31556926, "/"); // 1Year
     <div class="container-fluid">
     <div class="tb-height-b30 tb-height-lg-b30"></div>
       <div class="row">
-        <iframe id="ifm" name="ifm" width="100%" height="1120px" frameborder="0" src="https://sisense.edi-indonesia.co.id:8081/app/main#/dashboards/5df0f23e100c8c1110b2d08d/?embed=true&r=false&h=false&i=false" scrolling="auto"></iframe>
+        <iframe id="ifm" name="ifm" width="100%" height="1120px" frameborder="0" src="https://sisense.edi-indonesia.co.id:8081/api/demoSession/start/5df0f23e100c8c1110b2d08d/?embed=true&r=false&h=false&i=false" scrolling="auto"></iframe>
       </div><!-- .row -->
     </div>
     <!-- .container-fluid -->
